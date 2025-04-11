fe60dd8b42338dcaf76d1d3834f1db4f598026c9
Aastha-Bharti <asbharti27@gmail.com>
2025-04-11 18:19:46 +0530

    final

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 import passwordRouter from './router/passwordRouter';
 
 const app = express()
 app.use(express.json())
+app.use(cors());
 
 app.get("/" , async (req : Request ,res : Response) => {
     res.send("hello")
